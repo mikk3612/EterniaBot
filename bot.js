dc629ca5813698753c49c9c5644beb9a5ee0ebb5
mikk3612 <mikkelvedel@live.com>
2018-04-13 01:56:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'deadz') {
-    	message.reply('pong diiiiwd');
+    if (message.content === 'ping') {
+    	message.reply('pong');
   	}
 });
 
